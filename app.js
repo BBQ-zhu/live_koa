629a314d7110fe0ac5728fe8d40215c70dd1c2b5
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2022-01-07 09:18:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,96 +1,122 @@
 const Koa = require('koa')
 const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
+const fs = require('fs')
+const path = require('path')
+const Cors = require('koa2-cors') //跨域中间件
+const json = require('koa-json') //格式化json
+const logger = require('koa-logger') //日志输出
+const jsonerror = require('koa-json-error') //返回统一的错误提示
+const KoaBody = require('koa-body') //解析接口请求的参数
+const parameter = require('koa-parameter') //校验请求的参数
+const koajwt = require('koa-jwt'); //所有接口校验token
+const config = require('./config') //常量库
+const MongoConnect = require('./db')() //建立数据库连接
 
+const {scheduleCronstyle} = require('./controller/schedule') //定时任务(定时更新直播开播时间和状态)
+scheduleCronstyle()
+
+
+
+// const logs = require('./controller/logs') //操作日志中间件
+// app.use(logs())
+
+// 中间件对token进行验证 注意：放在路由前面
+app.use(koajwt({
+  secret: config.SECRET
+}).unless({
+  // 登录接口不需要验证
+  path: [ // 不需要做校验的接口在此处匹配  \/ 转译斜杠
+    /^\/api\/user\/loginuser/,
+    /^\/api\/user\/createUser/,
+    /^\/api\/roles\/findNavMenus/,
+    /^\/api\/roles\/findRole/,
+    /^\/api\/roles\/findOneRole/,
+    /^\/uploads\/userImgs/,
+    /^\/uploads\/schoolVideo/,
+    /^\/uploads\/scroImgs/,
+    /^\/uploads\/products/,
+    /^\/uploads\/icons/,
+    /^\/uploads\/contracts/,
+    /^\/api\/editor\/controller/,
+    /^\/upload\/ueditor\/image/,
+    /^\/upload\/ueditor\/image/,
+    /^\/api\/vipuser\/getCodeVipuser/,
+    /^\/api\/vipuser\/findVipuser/,
+    /^\/api\/vipuser\/updateVipuser/,
+    /^\/api\/vipuser\/createVipuser/,
+    /^\/api\/vipuser\/loginVipuser/,
+    /^\/uploads\/productLogo/,
+    /^\/api\/uploads\/findScrollImg/,
+    /^\/api\/uploads\/findProduct/,
+    /^\/api\/uploads\/findProductClass/,
+    /^\/api\/uploads\/findVideo/,
+    /^\/api\/uploads\/findRecruiting/,
+    /^\/api\/vipuser\/checkCodeVipuser/,
+    /^\/api\/logs\/createlogs/,
+    /^\/api\/user\/findUser/,
+    /^\/api\/contract\/uploadPhoneContractImg/,
+    /^\/api\/informations\/findNews/,
+    /^\/api\/service\/createIntegrate/,
+    /^\/api\/agents\/createAgent/,
+    /^\/api\/agents\/findAgent/,
+    /^\/api\/contract\/findContract/,
+    /^\/api\/statistics\/findStatistics/,
+    /^\/api\/statistics\/createStatistics/,
+    /^\/api\/statistics\/updateStatistics/,
+    /^\/testHtml/,
+  ]
+}));
+
+//路由引入
 const index = require('./routes/index')
 const users = require('./routes/users')
+const roles = require('./routes/roles')
+const uploads = require('./routes/uploads')
+const informations = require('./routes/informations')
+const product = require('./routes/product')
+const customer = require('./routes/customer')
+const integrate = require('./routes/integrate')
+const contract = require('./routes/contract')
+const agents = require('./routes/agents')
+const vipUser = require('./routes/Mobile/vipUser') //手机用户 
+const logs = require('./routes/logs')
+const internal = require('./routes/internal') 
+const mortgages = require('./routes/mortgages') 
+const statistic = require('./routes/statistic')
 
-
-// 直播服务
-const NodeMediaServer = require('node-media-server');
-
-// ffmpeg -re -i C:\Users\zWX990233\Desktop\video.mp4 -c copy -f flv rtmp://192.168.1.103:1935/live/home
-
-const config = {
-  rtmp: {
-    port: 1935,
-    chunk_size: 60000,
-    gop_cache: true,
-    ping: 30,
-    ping_timeout: 60
-  },
-  http: {
-    port: 8001,
-    mediaroot: './media',
-    allow_origin: '*'
-  },
-  trans: {
-    ffmpeg: './ffmpeg-gitHub/bin/ffmpeg.exe', // ffmpeg 播放器所在的路径
-    tasks: [
-      {
-        app: "live",
-        mp4: true,
-        mp4Flags: '[movflags=frag_keyframe+empty_moov]',//必须这样设置 
-        //'[movflags=frag_keyframe+empty_moov]'可以录制,//'[movflags=faststart]'不可以录制,
-        // hls: true,
-        // hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-        // dash: true,
-        // dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
-      }
-    ]
-  }
-};
-
-var nms = new NodeMediaServer(config)
-nms.run();
-
-
-nms.on('preConnect', (id, args) => {
-
-  console.log(1, '[直播服务中]', `id=${id} args=${JSON.stringify(args)}`);
-  // let session = nms.getSession(id);
-  // session.reject();
-});
-
-nms.on('postConnect', (id, args) => {
-  console.log(2, '[直播服务中]', `id=${id} args=${JSON.stringify(args)}`);
-});
-
-nms.on('doneConnect', (id, args) => {
-  console.log(3, '[直播服务中]', `id=${id} args=${JSON.stringify(args)}`);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-// error handler
-onerror(app)
-
-// middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+app.use(KoaBody({
+  formLimit:"50000mb",
+  jsonLimit:"50000mb",
+  // formidable:{
+  //   maxFileSize:2000*1024*1024 //设置文件最大限制，默认为2M
+  // }
 }))
+
+// app.use(KoaBody({ //放到路由下面，注意顺序
+//   multipart: true, //是否允许上传文件
+//   stict: true, //解析所有请求
+//   formidable: {
+//     // uploadDir:path.join(__dirname,'public/upload/'), // 设置文件上传目录,这里先不写，后面调用时动态创建目录
+//     keepExtensions: true, // 保持文件的后缀
+//     maxFileSize: 1024 * 1024 * 10 * 1024 // 设置上传文件大小最大限制，默认1G
+//   },
+//   onError: () => {
+//     ctx.body = {
+//       code: 400,
+//       data: '文件上传失败'
+//     }
+//   }
+// }));
+
+// error 错误处理
+app.use(Cors());
+app.use(jsonerror())
+app.use(parameter(app))
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
-app.use(views(__dirname + '/views', {
-  extension: 'ejs'
-}))
-
-// logger
+// logger打印接口请求日志 中间件
 app.use(async (ctx, next) => {
   const start = new Date()
   await next()
@@ -98,13 +124,25 @@ app.use(async (ctx, next) => {
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
-// routes
+// routes 路由注册
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
-
-// error-handling
+app.use(roles.routes(), users.allowedMethods())
+app.use(uploads.routes(), uploads.allowedMethods())
+app.use(informations.routes(), informations.allowedMethods())
+app.use(product.routes(), product.allowedMethods())
+app.use(customer.routes(), customer.allowedMethods())
+app.use(integrate.routes(), integrate.allowedMethods())
+app.use(contract.routes(), contract.allowedMethods())
+app.use(agents.routes(), agents.allowedMethods())
+app.use(vipUser.routes(), vipUser.allowedMethods())
+app.use(logs.routes(), logs.allowedMethods())
+app.use(internal.routes(), internal.allowedMethods())
+app.use(mortgages.routes(), mortgages.allowedMethods())
+app.use(statistic.routes(), statistic.allowedMethods())
+// error-handling 
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+  console.error('server error:', err, ctx)
 });
 
 module.exports = app
